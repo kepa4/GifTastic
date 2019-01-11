3f25341df325ddd047b5a19c150128ba141ff3a1
Keenan <kpautler4@gmail.com>
2019-01-10 17:03:17 -0700

    Make ajax calls

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,38 @@
 $(document).ready(function() {
   var array = [
-    'Ned Stark',
-    'Catelyn Stark',
-    'Jon Snow',
-    'Rob Stark',
-    'Arya Start',
-    'Sansa Stark',
-    'Bran Stark',
-    'Rickon Stark'
+    'Beagle',
+    'Basset',
+    'Dachshund',
+    'Bulldog',
+    'Poodle',
+    'Pug',
+    'Chihuahua',
+    'Husky',
   ];
 
   array.forEach(function(name) {
     $('.sidenav').append(`<button id=${name}>${name}</button>`);
   });
-  
-  $('button').on('click', function(e){
-                  $.ajax({
-                   type: 'GET',
-         
-                                  })   
-                  })
+
+  $('button').on('click', function(e) {
+    console.log(e);
+    var url =
+      'http://api.giphy.com/v1/gifs/search?q=' +
+      e.target.id +
+      '&api_key=o0kDV1eHMdbMcWplc4n285321HyV03He';
+    $.ajax({
+      method: 'GET',
+      url: url,
+      dataType: 'json',
+      success: function(data) {
+        console.log(data);
+        for(var i = 0; i < 10; i++){
+          $('.gifs').append(`<img src='${data.data[i].images.original.url}' alt='${data.data[i].title}'/>`)  
+        }
+      },
+      error: function(error) {
+        console.log(error);
+      },
+    });
+  });
 });
